ac765fd16405b9c96ba073e4f324738717878c59
hyooa <hyoyung3231@gmail.com>
2022-07-27 17:52:13 +0900

    하는 중

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,35 +1,64 @@
 import React from 'react';
-import './main.scss';
+import './main.css';
 
 const Main = () => {
     return (
         <div>
             <header>
-                <div id='header_top'>
-                    <div id='header_left'>
-                        <ul>
-                            <li>뭐</li>
-                            <li>라고</li>
-                            <li>하지</li>
-                        </ul>
-                    </div>
-                    <div id='header_img'>
-                        <img src='../image/logo.png' alt=''></img>
-                    </div>
-                    <div id='header_left'>
-                        <ul>
-                            <li>뭐</li>
-                            <li>라고</li>
-                            <li>하지</li>
-                        </ul>
-                    </div>
+                <div id='menu'>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <p>MENU</p>
+                </div>
+                <div id='menu_toggle'>
+                    <ul id='toggle'>
+                        <a href='/'><li>HOME</li></a>
+                        <a href='/'><li>Fixtures & Results</li></a>
+                        <a href='/'><li>PLAYER</li></a>
+                        <a href='/'><li>Contact Us</li></a>
+                        <a href='/'><li>FAQ</li></a>
+                        <a href='/'><li>SHOP</li></a>
+                    </ul>
                 </div>
             </header>
-            <div>
+
+            <div id='main'>
                 <div id='main_text'>
                     <h1>CHELSEA</h1>
                 </div>
+
+                <div id='daepyo'>
+                    <div id='Boehly'>
+                        <div>
+                            <div id='img'>
+                                <img src='./image/첼시_회장.png' alt=''></img>
+                                <p>Todd Boehly</p>
+                            </div>
+                            <p>
+                                Thomas Tuchel (German pronunciation born 29 August 1973) <br />
+                                is a German professional football manager and <br />
+                                former player who is the current head coach of Premier League club Chelsea.<br />
+                            </p>
+                        </div>
+                    </div>
+                    <div id='Tuchel'>
+                        <div>
+                            <div id='img'>
+                                <img src='./image/첼시_감독.png' alt=''></img>
+                                <p>Thomas Tuchel</p>
+                            </div>
+                            <p>
+                                Todd Boehly (born September 20, 1973) is an American businessman and investor.<br />
+                                He is also the interim CEO of the Hollywood Foreign Press Association and co-owner <br />
+                                and chairman of Premier League football club Chelsea.<br />
+                            </p>
+                        </div>
+                        
+                    </div>
+                </div>
             </div>
+
             <footer></footer>
         </div>
     );
